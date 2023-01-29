56c5d5242156d8180eb9046268d2db5bb309de93
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-01-29 18:35:54 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const axios2 = require("axios");
+const axios = require("axios");
 const app = express();
 const port = 3000;
+const BASE_URL = "";
+
 app.get("/games", (req, res) => {
   console.log(req);
   res.send("List of games");
